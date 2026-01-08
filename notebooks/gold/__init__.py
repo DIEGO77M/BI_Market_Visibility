a36b8ae126f91f931579e5155b62b1d543b77879
Diego Mayor <diego.mayorgacapera@gmail.com>
2026-01-07 23:24:47 -0500

    Update all project files after Databricks deployment and pipeline execution

---
+++ notebooks/gold/__init__.py
@@ -0,0 +1,17 @@
+"""
+BI Market Visibility
+Gold Layer Package
+
+This package contains Gold-layer logic:
+- Dimensions (SCD Type 2)
+- Static dimensions
+- Fact preparation helpers
+
+Author: Diego Mayorga
+"""
+
+__all__ = [
+    "dim_date",
+    "dim_product",
+    "dim_pdv"
+]
